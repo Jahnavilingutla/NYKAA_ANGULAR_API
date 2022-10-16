6902179f7963a516bd46373fef1978ab7816fed1
Jahnavilingutla <lingutlajahnavi19@gmail.com>
2022-10-16 15:49:53 +0530

    Add project files.

---
+++ Models/Registration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NYKAA_ANGULAR_API.Models
+{
+    public partial class Registration
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Useremail { get; set; }
+        public string? Password { get; set; }
+        public string? Mobile { get; set; }
+    }
+}
